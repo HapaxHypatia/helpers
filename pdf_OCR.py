c2f902272fba68c9b9c25b5cd5169a819534190f
HapaxHypatia <r.haskmann@gmail.com>
2022-03-26 13:14:28 +1000

    updated pdf OCR

--- pdf_OCR.py
+++ pdf_OCR.py
@@ -2,9 +2,8 @@ import pytesseract
 from pdf2image import convert_from_path
 import os
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-popplerPath = "C:\\projects\\VocabGrabber\\poppler-22.01.0\\Library\\bin"
-
+pytesseract.pytesseract.tesseract_cmd = "C:\\projects\\dependencies\\Tesseract-OCR\\tesseract.exe"
+popplerPath = "C:\\projects\\dependencies\\poppler-22.01.0\\Library\\bin"
 
 def pdf_ocr(DIR, LANG):
     """
@@ -13,9 +12,17 @@ def pdf_ocr(DIR, LANG):
     :param LANG: string of language for text recognition
     :return: none. new textfiles created in given directory
     """
-    for filename in os.listdir(DIR):
-        images = convert_from_path(DIR+filename, 350, poppler_path=popplerPath)
-        with open("{}txt".format(filename[:-3]), 'a') as f:
-            for image in images:
-                text = (str(pytesseract.image_to_string(image, lang=LANG)))
-                f.write(text)
+    files = os.listdir(DIR)
+    for file in files:
+        if file.endswith("pdf"):
+            #strip file extension to get string of filename
+            filename = file[:-4]
+            # if pdf has not been converted already
+            if "{}.txt".format(filename) not in files:
+                #convert to list of image files
+                images = convert_from_path(DIR+file, 350, poppler_path=popplerPath)
+                with open("{}\\{}.txt".format(DIR, filename), 'a') as f:
+                    # ocr each image in list
+                    for image in images:
+                        text = (str(pytesseract.image_to_string(image, lang=LANG)))
+                        f.write(text)
